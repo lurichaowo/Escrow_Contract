fbaaeb258b92cd397b260fcdbbd9b2a22450bf55
Kevin W <44531838+Darktwine@users.noreply.github.com>
2020-12-04 18:01:34 -0500

    Update HousingContract.sol

--- HousingContract.sol
+++ HousingContract.sol
@@ -2,6 +2,8 @@
 
 pragma solidity >=0.7.0 <0.8.0;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.1.0/contracts/token/ERC20/ERC20.sol";
+
 /** 
  * @title Owned
  * @dev Base contract to represent ownership of a contract
@@ -35,116 +37,41 @@ contract Mortal is Owned {
 }
 
 /** 
- * @title HousingContract
- * @dev Implements payment contract system between a landlord and tenents
+ * @title EscrowContract
+ * @dev Implements escrow system to serve as middleman between seller and buyer
  */
-contract HousingContract is Mortal {
-
-    struct Tenant {
-        uint256 id; // unique identifier for tenant
-        address payable account; // tenant's billing address 
-        uint256 leaseLength; // tenant's lease term in months
-    }
-
-    Tenant[] tenants; // list of currently occupied tenants, is dynamic array
-    uint256 currTenantCount; // current number of tenants
-    uint256 maxTenantCount; // maximum number of tenants allowed
-    uint256 securityDepositFee; // initial one-time refundable security deposit fee
-    uint256 rentCost; // montly rent cost
-    uint256 counter; // counter used to assign tenant ids
-
-    /** 
-    * @dev Create a new housing contract to collect rent from tenants
-    * @param _maxTenantCount maximum number of tenants allowed
-    * @param _securityDepositFee refundable security deposit fee
-    * @param _rentCost montly rent cost
-    */
-    constructor(
-        uint256 _maxTenantCount,
-        uint256 _securityDepositFee,
-        uint256 _rentCost
-    ) {
-        maxTenantCount = _maxTenantCount;
-        _securityDepositFee = securityDepositFee;
-        _rentCost = rentCost;
-        
-        currTenantCount = 0;
-        counter = 1;
-    }
-
-    //EVENTS
-    event RentPayment(
-        address indexed _from,
-        bytes32 indexed _id,
-        uint256 _value
+contract EscrowContract is Mortal {
+    uint256 tokens;
+    address public buyer;
+    address payable public seller;
+    
+    mapping ( address => uint256 ) public balances;
+    
+    event Purchase(
+        address _buyer,
+        uint256 _amount
     );
-
-    event SecurityDepositPayment(
-        address indexed _from,
-        bytes32 indexed _id,
-        uint256 _value
-    );
-
-    event SecurityDepositRefund(
-        address indexed _to,
-        bytes32 indexed _id,
-        uint256 _value
-    );
-
-    /** 
-    * @dev Changes monthly rent cost
-    * @param newRentCost new montly rent cost
-    */
-    function changeRent(uint256 newRentCost) public onlyOwner {
-        rentCost = newRentCost;
+    
+    constructor(address _buyer, address payable _seller) public {
+    buyer = _buyer;
+    seller = _seller;
     }
+    
+    // seller giving arbiter(the smart contract) tokens they want to sell, through approval and deposit
+    ERC20(seller).approve(address(this), uint tokens);
+    
+    deposit(uint tokens) {
+    // add the deposited tokens into the existing balance of the smart contract
+    balances[msg.sender]+= tokens;
 
-    /** 
-    * @dev Charges montly rent to all currently occipied tenants
-    */
-    function chargeRent() public onlyOwner {}
-
-    /** 
-    * @dev Adds tenant to list of tenants and charges security deposit fee
-    * @param newTenant payment address of new tenant 
-    * @param leaseL tenant's lease term in months
-    * newID Generates new Tenant to be added to array of tenants
-    */
-    function addTenant(address newTenant, uint256 leaseL) public onlyOwner {
-        /** Check if can house anymore tenants, revert() state of EVM otherwise */
-        require(tenants.length < maxTenantCount);
-
-        uint256 newID = generateNewID(); 
-        Tenant memory t = Tenant(newID, newTenant, leaseL);
-        tenants.push(t);
+    // transfer the tokens from the sender to this contract
+    ERC20(seller).transferFrom(msg.sender, arbiter, tokens);
     }
-
-    /** 
-    * @dev Removes tenant from list of tenants and refunds security deposit fee
-    * @param tenantId id of tenant to be removed
-    * using swap and delete since no pop function exists
-    */
-    function removeTenant(uint256 tenantId) public onlyOwner {
-        /** Find correct tenant by ID */
-        for (uint i = 0; i < tenants.length; i++){
-            if (tenants[i].id == tenantId){
-                /** Refund security deposit */
-                tenants[i].account.transfer(securityDepositFee);
-
-                Tenant memory t = tenants[i];
-                tenants[i] = tenants[tenants.length-1];
-                tenants[tenants.length-1] = t;
-                tenants.pop();
-                
-                break;
-            }
-        }
-    }
-
-    /** 
-    * @dev Generates new unique ID for new tenant, to be called by addTenant function
-    */
-    function generateNewID() public onlyOwner{
-        return;
-    }
+    
+    // when funds are received from buyer, give tokens to buyer and money to seller
+    emit Purchase(msg.sender, address(this).balance)
+    
+    ERC20(arbiter).transfer(msg.sender, balances[msg.sender]);
+    seller.transfer(address(this).balance);
+    
 }
