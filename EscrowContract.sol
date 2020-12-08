0346d6308e4eece5d3177e57ad531684a258fc93
Kevin W <44531838+Darktwine@users.noreply.github.com>
2020-12-07 23:26:25 -0500

    Update EscrowContract.sol

--- EscrowContract.sol
+++ EscrowContract.sol
@@ -3,6 +3,7 @@
 pragma solidity >=0.7.0 <0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.3/contracts/token/ERC20/ERC20.sol";
+
 /** 
  * @title Owned
  * @dev Base contract to represent ownership of a contract
@@ -15,6 +16,7 @@ contract Owned {
 	constructor() {
 		owner = msg.sender;
 	}
+	
 	// Access control modifier
 	modifier onlyOwner {
 		require(msg.sender == owner,
@@ -22,6 +24,7 @@ contract Owned {
 		_;
 	}
 }
+
 
 /** 
  * @title Mortal
@@ -35,160 +38,54 @@ contract Mortal is Owned {
 	}
 }
 
-
-contract Token is ERC20 {
-    constructor () public ERC20("Token", "TKN") {
-        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
-    }
-}
-
 /** 
- * @title EscrowContract
+ * @title TokenEscrowContract
  * @dev Implements escrow system to serve as middleman between seller and buyer
  */
-contract EscrowContract is Mortal {
-
-    enum paymentStatus {Pending, Completed}
+contract TokenEscrowContract is ERC20, Mortal {
     
-    ERC20 public tokens;
-    mapping ( address => uint256 ) public balances;
+    mapping(address => uint256) private tokenBalances;
+    mapping(address => uint256) private etherBalances;
     
-    event ItemPosted(
-        uint256 indexed _id,
-        address indexed _seller,
-        uint256 _amount,
-        paymentStatus status
-    );
-
-    event ItemSold(
-        uint256 indexed _id,
-        address indexed _buyer,
-        uint256 _amount,
-        paymentStatus status
-    );
-
-    struct Item{
-        uint256 _id; // unique item number 
-        uint256 _amount; // # of tokens
-        uint256 _price; // price of each token(s)
-        address payable _seller;
+    constructor() public ERC20("Token", "TKN") {
+        _mint(msg.sender, 1000000);
     }
 
-    // Global Variables
-    Item[] items; // list of items on sale 
-    uint256 _id_counter = 0; // for unique item number 
-
-
-    /** 
-    * @dev Seller calls function and sets up shop and deposit tokens onto arbitar 
-    * @param _seller address of seller to be stored on item 
-    * @param _amount amount of tokens to be sold 
-    * @param _sell_price sell price of each token
-    */
-    function sellTokens(address payable _seller, uint _amount, uint256 _sell_price) public payable {
-        
+    function depositTokens(uint256 _tokens) public {
+        ERC20.approve(msg.sender, _tokens);
+    
         // transfer the tokens from the sender to this contract
-        ERC20(_seller).approve(address(this), _amount);
-        ERC20(_seller).increaseAllowance(address(this), _amount);
-        ERC20(_seller).transferFrom(_seller, address(this), _amount);
+        ERC20.transferFrom(msg.sender, address(this), _tokens);
         
-        // add the deposited tokens into existing balance 
-        balances[msg.sender] += tokens;
-
-        // create new item to be added to list of items[]
-        Item memory i = Item(_id_counter, _amount, _sell_price, _seller);
-        items.push(i);
-        
-        // event call 
-        emit ItemPosted(_id_counter, _seller, _amount, paymentStatus.Pending);
-        
-        // increment _id_counter
-        _id_counter++;
-    }
-
-    /** 
-    * @dev Buyer requests to see what is being sold (will be displayed on console)
-    */
-    function viewItems() public{
-        // parse through and print onto console
-        if (items.length > 0){
-            for (uint i = 0; i < items.length; i++){
-                log2(
-                    bytes32(items[i]._id),
-                    bytes32(items[i]._amount),
-                    bytes32(items[i]._price)
-                );
-            }
-        }
-    }
-
-    /** 
-    * @dev Buyer enters item number of token, they want to buy
-    * @param _id number to identify which item they want to buy 
-    * @param _buyer address of buyer to transfer ownership of token
-    */
-    function buyItem(uint256 _id, address payable _buyer) public payable{
-        // find tokens associated with _id
-        uint index; // index of item
-        bool found; // if item actually exists 
-        (index, found) = findItem(_id); // function to find item
-        require(found == true, "Item is not found"); // check if item actually exits, else error msg
-        
-        Item memory itemToBeBought = items[index]; // item obj of item to be purchased
-        
-        require(msg.value >= itemToBeBought._price * itemToBeBought._amount, "Insufficient funds to allow purchase of item"); // check if buyer sent enough ether to buy tokens
-
-        // return the change amount of ether sent
-        if (msg.value > itemToBeBought._price * itemToBeBought._amount){
-            uint diff = msg.value - itemToBeBought._price * itemToBeBought._amount;
-            _buyer.transfer(diff);
-        }
-        
-        // sends tokens to buyer
-        ERC20(address(this)).transfer(_buyer, itemToBeBought._amount);
-
-        // send ether to 
-        itemToBeBought._seller.transfer(msg.value);
-        
-        // event call 
-        emit ItemSold(_id_counter, _buyer, itemToBeBought._amount, paymentStatus.Completed);
-    }
-
-    /** 
-    * @dev Seller wants to remove token(s) from list 
-    * @param _id number to identify which item they want to remove 
-    */
-    function removeItem(uint256 _id) public {
-        // find tokens associated with _id
-        uint index; // index of item
-        bool found; // if item actually exists 
-        (index, found) = findItem(_id); // function to find item
-        require(found == true, "Item is not found"); // check if item actually exits, else error msg
-        
-        Item memory itemToRemove = items[index]; // item obj to remove
-
-        require(msg.sender == itemToRemove._seller, "Not authorized to remove sale of item"); // check if is actually seller of tokens
-
-        ERC20(address(this)).transfer(itemToRemove._seller, itemToRemove._amount); // transfer tokens back to original seller
-        
-        // swap and pop item from array of items[]  
-        items[index] = items[items.length-1];
-        items[items.length-1] = itemToRemove;
-        items.pop();
+        // add the deposited tokens into existing balance, records how much the sender has put into the smart contract 
+        tokenBalances[msg.sender] += _tokens;
     }
     
-    /** 
-    * @dev To find and return the index in items[] of the item being looked for through item's _id
-    * @param _id number to identify which item they want to find
-    */
-    function findItem(uint256 _id) public view returns (uint, bool){
-        uint itemToFind;
-        // find the item 
-        for (uint i = 0; i < items.length; i++){
-            if (items[i]._id == _id){
-                return (itemToFind, true);
-            }
-        }
-        return (0, false);
+    function depositEther(uint256 _etherAmount) payable public {
+        require(msg.value == _etherAmount);
+        etherBalances[msg.sender] += _etherAmount;
     }
+    
+    function buyTokens(address _seller, address _buyer, uint256 _tokens, uint256 _etherAmount) public {
+        require(etherBalances[_buyer] >= _etherAmount, "Error: Not enough ether balance to buy tokens.");
+        etherBalances[_buyer] -= _etherAmount;
+        etherBalances[_seller] += _etherAmount;
+        
+        tokenBalances[_seller] -= _tokens;
+        tokenBalances[_buyer] += _tokens;
+    }
+    
+    function withdrawTokens() public {
+        tokenBalances[msg.sender] = 0;
+        ERC20.transfer(msg.sender, tokenBalances[msg.sender]);
+    }
+    
+    function tokenBalancesOf(address _requester) public view returns (uint256) {
+        return tokenBalances[_requester];
+    }
+    
+    function etherBalancesOf(address _requester) public view returns (uint256) {
+        return etherBalances[_requester];
+    }
+  
 }
